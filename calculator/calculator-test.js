d9907b02312bfbe031cf9e7abd48856da4bfb7ec
Quentin Ikeno <ikenoq@gmail.com>
2021-12-17 20:39:01 -1000

    make calculator function and write tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,42 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it("should calculate the monthly rate correctly", function () {
+	expect(
+		+calculateMonthlyPayment({
+			amount: 10000,
+			years: 10,
+			rate: 0.06,
+		})
+	).toEqual(111.02);
+	expect(
+		+calculateMonthlyPayment({
+			amount: 200000,
+			years: 30,
+			rate: 0.03,
+		})
+	).toEqual(843.21);
+	expect(
+		+calculateMonthlyPayment({
+			amount: 15000,
+			years: 5,
+			rate: 0.07,
+		})
+	).toEqual(297.02);
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it("should return a result with 2 decimal places", function () {
+	expect(
+		calculateMonthlyPayment({
+			amount: 10000,
+			years: 10,
+			rate: 0.06,
+		})
+	).toMatch(/^\d+\.\d\d$/);
+	expect(
+		calculateMonthlyPayment({
+			amount: 200000,
+			years: 30,
+			rate: 0.03,
+		})
+	).toMatch(/^\d+\.\d\d$/);
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,40 +1,58 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById("calc-form");
-  if (form) {
-    setupIntialValues();
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      update();
-    });
-  }
+window.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("calc-form");
+
+	if (form) {
+		setupIntialValues();
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
+			update();
+		});
+	}
 });
 
 function getCurrentUIValues() {
-  return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+	return {
+		amount: +document.getElementById("loan-amount").value,
+		years: +document.getElementById("loan-years").value,
+		rate: +document.getElementById("loan-rate").value,
+	};
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+	const amountInput = document.getElementById("loan-amount");
+	const yearsInput = document.getElementById("loan-years");
+	const rateInput = document.getElementById("loan-rate");
+	amountInput.value = 10000;
+	yearsInput.value = 10;
+	rateInput.value = 0.06;
+	update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+	const values = getCurrentUIValues();
+	const monthly = calculateMonthlyPayment(values);
+	updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+	const { amount, years, rate } = values;
+	const i = rate / 12;
+	const n = years * 12;
+	const payment = (amount * i) / (1 - (1 + i) ** -n);
+	return (Math.round(payment * 100) / 100).toString();
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+	const monthlyPaymentSpan = document.getElementById("monthly-payment");
+	monthlyPaymentSpan.innerText = monthly;
 }
